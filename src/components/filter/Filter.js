7c7cb3408987bdc7d1adeebb6b9b4dd55faae5dd
lfortin@adneom.com <lfortin@adneom.com>
2019-07-04 15:40:10 +0200

    feat(): create components

---
+++ src/components/filter/Filter.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Filter = () => {
+  console.log('Filter component');
+  return; /* <div>{test}</div>;*/
+};
+
+export default Filter;
---
+++ src/components/profile/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Profile = () => {
+  const test = 'PROFILE';
+  console.log(test);
+  return <div>{test}</div>;
+};
+
+export default Profile;
